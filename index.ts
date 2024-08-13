acba54776ee932d766c680fdca4ad096a32421c6
wanyuasam <samuelwanyua@gmail.com>
2024-08-13 09:32:57 +0300

    Interfaces in typescript

--- index.ts
+++ index.ts
@@ -2,15 +2,15 @@
 interface UserInterface{
     id: number,
     name: string,
-    age: number
+    age?: number // age now is optional
 }
 const User: UserInterface ={
     id: 2,
     name: 'sam',
     age: 23,
-};
+};User.age = 34;
 
-User.age = 34;
+
 console.log(User.age);
 
 
