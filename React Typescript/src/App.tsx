6222334c4777766a311296d3ff13584c9c863ddd
wanyuasam <samuelwanyua@gmail.com>
2024-08-13 14:34:17 +0300

    Initiate a shopping cart project

--- React Typescript/src/App.tsx
+++ React Typescript/src/App.tsx
@@ -1,17 +1,20 @@
-import './App.css'
-import {User} from './User'
-
-
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
 
 function App() {
  
 
 
   return (
-        <>
-          <User name = {"Wesly"} age = {24} isMarried = {false} />
-          <User name = {"Richard"} age = {56} isMarried = {true} />
-        </>
+        <Container>
+          <Routes>
+            <Route  path="/" element={<Home />}/>
+            <Route  path="/store" element={<Store />}/>
+            <Route  path="/about" element={<About />}/>
+          </Routes>
+        </Container>
   )
 }
 
