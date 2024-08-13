f88a2014a925eb0d826822340e661193bc981df6
wanyuasam <samuelwanyua@gmail.com>
2024-08-13 09:21:44 +0300

    FUnctions in typescript

---
+++ tut.js
@@ -0,0 +1,15 @@
+var id = 4; // js
+var num = 23; // typescript
+var company = "Acne corp.";
+var isTrue = true;
+// define array
+var position = ['1', '2', '3', '4', '5'];
+var x = "pedro";
+var xArr = ["Pedro", 0, true];
+// any - defines a variable in any of the types
+// defining a function
+var concantenateValues = function (a, b) {
+    return a + b;
+};
+var result = concantenateValues("sam", "wanyua");
+console.log(result);
--- tut.ts
+++ tut.ts
@@ -8,3 +8,12 @@ let position: string[] = ['1','2','3','4','5'];
 let x: any  = "pedro";
 let xArr: any[] = ["Pedro", 0, true];
 
+// any - defines a variable in any of the types
+
+
+// defining a function
+const concantenateValues = (a:string,b: string) => {
+    return a + b;
+}
+const result: string = concantenateValues("sam", "wanyua");
+console.log(result);
