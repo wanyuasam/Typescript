c1f92d6effb5269ae46e3c65dcfdeb2b30d38f17
wanyuasam <samuelwanyua@gmail.com>
2024-08-13 09:42:17 +0300

    Passing functions as object in typescript

--- index.ts
+++ index.ts
@@ -3,14 +3,20 @@ interface UserInterface{
     id: number,
     name: string,
     age?: number // age now is optional
+    greet(message: string): void // we're not returning anything
 }
 const User: UserInterface ={
     id: 2,
     name: 'sam',
     age: 23,
+    greet(message: string): void {
+        console.log(message)
+    }
 };
 
 User.age = 34;
+
+User.greet("I am a TS developer");
 
 if(!User.age){
     console.log("Age not found!")
