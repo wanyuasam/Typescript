cfd84c01d781ce49b5816ba80d9ae841d31eda0d
wanyuasam <samuelwanyua@gmail.com>
2024-08-13 14:39:53 +0300

    Initiate a shopping cart project

--- React Typescript/src/App.tsx
+++ React Typescript/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
+import { About } from "./pages/About"
 
 function App() {
  
