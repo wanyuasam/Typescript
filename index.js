c129e46ce65f5672b785d1119592d35d68ce4fe2
wanyuasam <samuelwanyua@gmail.com>
2024-08-13 09:43:27 +0300

    Passing functions as object in typescript

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var User = {
+"use strict";
+const User = {
     id: 2,
     name: 'sam',
     age: 23,
+    greet(message) {
+        console.log(message);
+    }
 };
 User.age = 34;
-console.log(User.age);
+User.greet("I am a TS developer");
+if (!User.age) {
+    console.log("Age not found!");
+}
+else {
+    console.log(User.age);
+}
--- tut.js
+++ tut.js
@@ -1,15 +1,16 @@
-var id = 4; // js
-var num = 23; // typescript
-var company = "Acne corp.";
-var isTrue = true;
+"use strict";
+let id = 4; // js
+let num = 23; // typescript
+let company = "Acne corp.";
+let isTrue = true;
 // define array
-var position = ['1', '2', '3', '4', '5'];
-var x = "pedro";
-var xArr = ["Pedro", 0, true];
+let position = ['1', '2', '3', '4', '5'];
+let x = "pedro";
+let xArr = ["Pedro", 0, true];
 // any - defines a variable in any of the types
 // defining a function
-var concantenateValues = function (a, b) {
+const concantenateValues = (a, b) => {
     return a + b;
 };
-var result = concantenateValues("sam", "wanyua");
+const result = concantenateValues("sam", "wanyua");
 console.log(result);
