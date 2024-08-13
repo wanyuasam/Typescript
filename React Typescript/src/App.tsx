b6a440fc6fac7d3bdce2baf7cfcdf1f2fed3ceab
wanyuasam <samuelwanyua@gmail.com>
2024-08-13 12:46:34 +0300

    Use hooks in React Typescript

--- React Typescript/src/App.tsx
+++ React Typescript/src/App.tsx
@@ -5,26 +5,17 @@ import {Person} from './Person'
 
 
 function App() {
-  const [showInfo, setShowInfo] = useState<boolean | null>(false);
-  const toggleInfo = () => {
-    setShowInfo(prev => !prev);
-  }
+ 
 
 
   return (
-    <div>
-      {
-      showInfo && (
         <>
           <Person name = {"Wesly"} age = {24} isMarried = {false} />
           <Person name = {"Richard"} age = {56} isMarried = {true} />
+          <Person name = {"Richard"} age = {56} isMarried = {true} />
+          <Person name = {"Richard"} age = {56} isMarried = {true} />
+          <Person name = {"Richard"} age = {56} isMarried = {true} />
         </>
-      )
-    }
-    <button  onClick={toggleInfo}> Toggle Info</button>
-    </div>
-    
-    
   )
 }
 
--- React Typescript/src/Person.tsx
+++ React Typescript/src/Person.tsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
+
 // interface
 interface Person{
     name: string
     age: number
     isMarried: boolean
 }
- export const Person = (props: Person) => {
+
+export const Person = (props: Person) => {
+    const [showInfo, setShowInfo] = useState<boolean | null>(false);
+    const toggleInfo = () => {
+      setShowInfo(prev => !prev);
+    }
+
   return (
     <div>
-        <p>Name: {props.name}</p>
-        <p>Age: {props.age}</p>
-        <p>This person is {props.isMarried ? "is married" : "is single"} </p>
+        <button onClick={toggleInfo}>Toggle Info</button>
+        {showInfo && (
+            <>
+                <p>Name: {props.name}</p>
+                <p>Age: {props.age}</p>
+                <p>This person is {props.isMarried ? "is married" : "is single"} </p>
+            </>
+        
+        )}
     </div>
-  )
-}
-
-export default Person
+  )}
+export default Person;
