2ef9e8234a96aabb725b03b974050859e3303aed
wanyuasam <samuelwanyua@gmail.com>
2024-08-13 12:20:10 +0300

    Use props in React Typescript

--- React Typescript/src/App.tsx
+++ React Typescript/src/App.tsx
@@ -4,11 +4,14 @@ import {Person} from './Person'
 
 
 function App() {
+  const fetchUser = () => ({name: "Kelvin HartKing", age: 45, isMarried: null});
+  const userFetched = fetchUser();
 
   return (
     <>
-     <Person name = {"Wanyua"} age = {32} isMarried = {false} />
+     <Person name = {"Wesly"} age = {24} isMarried = {false} />
      <Person name = {"Richard"} age = {56} isMarried = {true} />
+     <Person name = {userFetched.name} age = {56} isMarried = {true} />
     </>
   )
 }
--- React Typescript/src/Person.tsx
+++ React Typescript/src/Person.tsx
@@ -1,10 +1,10 @@
 // interface
-interface Props{
+interface Person{
     name: string
     age: number
     isMarried: boolean
 }
- export const Person = (props: Props) => {
+ export const Person = (props: Person) => {
   return (
     <div>
         <p>Name: {props.name}</p>
